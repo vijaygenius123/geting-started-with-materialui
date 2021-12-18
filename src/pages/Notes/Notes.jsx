82dd9a7dc243e66ddcf786cbccd7fe76edd9aeed
Vijayaraghavan Sundararaman <vijaygenius123@gmail.com>
2021-12-18 13:33:23 +0000

    Display notes from backend

--- src/pages/Notes/Notes.jsx
+++ src/pages/Notes/Notes.jsx
@@ -1,7 +1,24 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from 'axios';
+import Typography from "@mui/material/Typography";
+import Container from "@mui/material/Container";
 
 const Notes = () => {
-    return <h1>Notes</h1>
+    const [notes, setNotes] = useState([])
+
+    useEffect(() => {
+        axios.get(`${process.env.REACT_APP_BASE_URL}/notes`)
+            .then(resp => setNotes(resp.data))
+    }, [])
+
+
+    return <Container>
+        <Typography variant="h6" component="h2" gutterBottom color="textSecondary">
+            Notes
+        </Typography>
+        {notes.map(note => <Typography>{note.title}</Typography>)}
+
+    </Container>
 }
 
 export default Notes
